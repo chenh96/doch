2439b65f46a1130466d7e16d49eae6e51beb7abb
Han Chen <chenh96@foxmail.com>
2022-04-22 11:32:48 +0800

    Bug fix

--- doch/src/main/java/cloud/chenh/doch/data/base/BaseRepository.java
+++
@@ -1,9 +0,0 @@
-package cloud.chenh.doch.data.base;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface BaseRepository<T extends BaseEntity> extends JpaRepository<T, Long> {
-
-}
